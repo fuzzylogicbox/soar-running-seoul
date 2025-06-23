25f16939c9d1869d2f63ede3780e97c4b2eea544
fuzzylogicbox <fuzzylogicbox@gmail.com>
2025-06-23 17:54:23 +0900

    feat: Initial project setup #1

---
+++ src/main/java/io/github/fuzzylogicbox/soarrunningseoul/SoarRunningSeoulApplication.java
@@ -0,0 +1,13 @@
+package io.github.fuzzylogicbox.soarrunningseoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoarRunningSeoulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoarRunningSeoulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/fuzzylogicbox/soarrunningseoul/SoarRunningSeoulApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.fuzzylogicbox.soarrunningseoul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoarRunningSeoulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
