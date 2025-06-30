88854a92394a417cac1a9f3823b764dec2526f82
fuzzylogicbox <fuzzylogicbox@gmail.com>
2025-06-30 16:31:52 +0900

    feat: Implement Question entity, DTOs and repository

--- src/main/java/io/github/fuzzylogicbox/soarrunningseoul/domain/Question.java
+++ src/main/java/io/github/fuzzylogicbox/soarrunningseoul/domain/Question.java
@@ -1,4 +1,44 @@
+
 package io.github.fuzzylogicbox.soarrunningseoul.domain;
 
-public class Question {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "questions")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@SuperBuilder
+public class Question extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(name = "status", nullable = false, length = 20)
+    private String status;
+
+//    @OneToOne(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private Answer answer;
 }
--- src/main/java/io/github/fuzzylogicbox/soarrunningseoul/repository/QuestionRepository.java
+++ src/main/java/io/github/fuzzylogicbox/soarrunningseoul/repository/QuestionRepository.java
@@ -1,4 +1,7 @@
 package io.github.fuzzylogicbox.soarrunningseoul.repository;
 
-public class QuestionRepository {
+import io.github.fuzzylogicbox.soarrunningseoul.domain.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
 }
