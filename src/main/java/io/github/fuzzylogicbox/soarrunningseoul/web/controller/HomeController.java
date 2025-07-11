cdda0490dc983af4db71093106b29e06f1caa63f
fuzzylogicbox <fuzzylogicbox@gmail.com>
2025-07-11 20:02:31 +0900

    refactor: move HomeController to web.controller package

--- src/main/java/io/github/fuzzylogicbox/soarrunningseoul/web/controller/HomeController.java
+++ src/main/java/io/github/fuzzylogicbox/soarrunningseoul/web/controller/HomeController.java
@@ -1,4 +1,4 @@
-package io.github.fuzzylogicbox.soarrunningseoul.web;
+package io.github.fuzzylogicbox.soarrunningseoul.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
