a6381eed7a497e1917f08390569f75024877e058
fuzzylogicbox <fuzzylogicbox@gmail.com>
2025-07-29 18:14:12 +0900

    feat: add about UI and styling

--- src/main/java/io/github/fuzzylogicbox/soarrunningseoul/web/controller/AboutController.java
+++ src/main/java/io/github/fuzzylogicbox/soarrunningseoul/web/controller/AboutController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class aboutController {
+public class AboutController {
 
     @GetMapping("/about")
     public String about () {
